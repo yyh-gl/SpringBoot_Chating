65a06933de1f0f38a711ae8762d7722bfffd7d99
yupouri <yhonda@mikilab.doshisha.ac.jp>
2017-10-18 20:33:49 +0900

    Modify name : login -> chatroom

--- src/main/java/com/yupouri/Controller/ChatRoomController.java
+++ src/main/java/com/yupouri/Controller/ChatRoomController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class LoginController {
+public class ChatRoomController {
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView showLoginForm(ModelAndView mav) {
